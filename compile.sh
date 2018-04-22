18bb4154e041192c9753e83d7a341c7f9f865066
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-04-22 14:12:52 +0200

    2

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+git clone https://github.com/tdlib/td
+cd td
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j4
+sudo make install
+cd ../../
+git clone https://github.com/giuseppem99/tdlua
+mkdir build
+cp uploadtravis.lua build/
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DTDLUA_TD_STATIC=1 ../tdlua
+cmake --build .
+
+curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so"
+lua ../examples/uploadtravis.lua
