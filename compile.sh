9bf3954264f24aa84f9838ca2f4c83239b15a4ea
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-06-29 15:18:04 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@ git clone https://github.com/tdlib/td
 cd td
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
 make -j4 VERBOSE=1
 sudo make install
 cd ../../
@@ -11,7 +11,7 @@ git clone https://github.com/giuseppem99/tdlua
 mkdir build
 cp uploadtravis.lua build/
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DTDLUA_TD_STATIC=1 ../tdlua
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DTDLUA_TD_STATIC=1 ../tdlua
 cmake --build .
 
 curl -s https://api.telegram.org/bot$token/sendDocument -F chat_id=68972553 -F document="@tdlua.so"
